a9deef2928af31aef705175ea08f21e5df50b4dd
starlab-pchavan <33275219+starlab-pchavan@users.noreply.github.com>
2017-12-21 16:43:18 +0530

    upload files via website

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Learn Git</title>
+	<!-- Custome Stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+			<!-- header -->
+			<header>
+				<h1>Learn Git Commands</h1>
+
+				<div class="nav">
+					<ul>
+						<li>
+							<a href="#">Home</a>
+							<a href="#">Portfolio</a>
+							<a href="#">About Us</a>
+							<a href="#">Contact Us</a>
+						</li>
+					</ul>
+				</div>
+				<hr>
+			</header>
+			<!-- main body -->
+			<div class="content">
+				<div class="col-md-12">
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				</div>
+			</div>
+			<footer>
+				<hr>
+				<p>LearnGit &copy; All Rights Reserved <?php echo date("Y"); ?></p>	
+			</footer>
+	</div>
+</body>
+</html>
